44cdfb75b881f7885c8fd1b158f69d9c795dc69d
root <root@DESKTOP-DHALUET>
2023-04-05 21:46:38 -0500

    archiivo 1

---
+++ Functions-algorithms-loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int _isdigit(int c){
+if (c= 0, c>=9);
+    return(1);
+    
+    {if (c>= 10)
+    {
+        return(0);
+    }
+     (c>= 10);
+    return(0);
+     }
+     }
