4f66bfad352f95fbe024cf41214b4316761ffc01
root <root@DESKTOP-DHALUET>
2023-05-01 22:25:11 -0500

    archivo 3

---
+++ More-pointers-arrays-and-string/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int i, j;
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = 0; accept[j] != '\0'; j++) {
+            if (s[i] == accept[j])
+                break;
+        }
+        if (accept[j] == '\0')
+            return i;
+    }
+    return i;
+}
